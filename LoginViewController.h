aa17ad82e7c5b46f96a836f2f13623ebcdf499b0
chenyishain <diligent1230@hotmail.com>
2015-08-21 16:57:22 +0800

    second commit

---
+++ LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Bento++
+//
+//  Created by yishain on 8/21/15.
+//  Copyright (c) 2015 yishain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ LoginViewController.m
@@ -0,0 +1,39 @@
+//
+//  LoginViewController.m
+//  Bento++
+//
+//  Created by yishain on 8/21/15.
+//  Copyright (c) 2015 yishain. All rights reserved.
+//
+
+#import "LoginViewController.h"
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
