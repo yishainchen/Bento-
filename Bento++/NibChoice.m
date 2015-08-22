63fb13cdb8066a6644b83771b7ca63c26ffabeab
peoplewhite <shonshon7@gmail.com>
2015-08-23 05:16:05 +0800

    add class file on it

---
+++ Bento++/NibChoice.m
@@ -0,0 +1,14 @@
+//
+//  NibChoice.m
+//  Bento++
+//
+//  Created by sean on 2015/8/23.
+//  Copyright (c) 2015年 yishain. All rights reserved.
+//
+
+#import "NibChoice.h"
+
+@implementation NibChoice
+
+
+@end
